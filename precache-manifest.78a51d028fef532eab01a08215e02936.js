01b588ba25423e5d2760f170415b3f34f681aea0
Evandro Rodrigues <43556689+evanrodr@users.noreply.github.com>
2020-01-28 08:38:53 -0300

    Updates

---
+++ precache-manifest.78a51d028fef532eab01a08215e02936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c223198a92cfe80de5c79ce9dd889ec6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3594059502a5d6a648bd",
+    "url": "/robofriends/static/css/main.184ef322.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3594059502a5d6a648bd",
+    "url": "/robofriends/static/js/main.a12d95b8.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
